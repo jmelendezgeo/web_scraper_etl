2979fd53cb7ff0d944631c826eef920c86cd704b
jmelendez <jrmelendez.martin@gmail.com>
2021-08-01 16:07:22 -0400

    Version inicial. Funciona con universal

---
+++ transform/main.py
@@ -0,0 +1,130 @@
+import argparse
+import logging
+logging.basicConfig(level = logging.INFO)
+from urllib.parse import urlparse
+import re
+import hashlib
+import nltk
+from nltk.corpus import stopwords
+
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+
+def main(filename):
+    logger.info('Starting cleaning process')
+    
+    df = _read_data(filename)
+    newspaper_uid = _extract_newspaper_uid(filename)
+    df = _add_newspaper_uid_column(df,newspaper_uid)
+    df = _extract_host(df)
+    df = _clean_multiple_spacing(df,'title')
+    df = _clean_multiple_spacing(df,'body')
+    df = _fill_missing_titles(df)
+    df = _generate_uids_for_rows(df)
+    df = _remove_new_lines_character(df,'body')
+    df = _remove_new_lines_character(df,'title')
+    df = _tokenize_column(df,'title')
+    df = _tokenize_column(df,'body')
+    
+    _save_data(df,filename)
+    
+    
+    return df
+    
+    
+    
+def _add_newspaper_uid_column(df,newspaper_uid):
+    logger.info('Filling newspaper_uid column with {}'.format(newspaper_uid))
+    df['newspaper_uid'] = newspaper_uid
+    return df
+
+    
+def _read_data(filename):
+    logger.info(f'Reading file {filename}')
+    
+    return pd.read_csv(filename)
+
+
+def _clean_multiple_spacing(df,column_name):
+    logger.info(f'Cleaning multiple spacing from {column_name}')
+    df[column_name] = df[column_name].str.replace('  ','')
+    return df
+
+def _extract_newspaper_uid(filename):
+    logger.info('Extracting newspaper uid')
+    newspaper_uid = filename.split('_')[0]
+    
+    logger.info('Newspaper uid detected: {}'.format(newspaper_uid))
+    return newspaper_uid
+
+
+def _extract_host(df):
+    logger.info('Extracting host from urls')
+    df['host'] = df['url'].apply(lambda url: urlparse(url).netloc)
+    
+    return df
+
+def _fill_missing_titles(df):
+    logger.info('Filling missing titles:')
+    missing_titles_mask = df['title'].isna()
+    
+    missing_titles = (df[missing_titles_mask]['url']
+                       .str.extract(r'(?P<missing_titles>[^/]+)$')
+                       .applymap(lambda title: title.replace('-',' '))
+                       .applymap(lambda title: title.capitalize()))
+    
+    df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:,'missing_titles']
+    
+    return df
+
+def _generate_uids_for_rows(df):
+    logger.info('Generating uids for each row')
+    uids = (df
+            .apply(lambda row: hashlib.md5(bytes(row['url'].encode())),axis=1)
+            .apply(lambda hash_object: hash_object.hexdigest())
+            )
+    df['uid'] = uids
+    
+    return df.set_index('uid')
+
+def _remove_new_lines_character(df,column_name):
+    logger.info(f'Remove new lines from {column_name}')
+    stripped_column = (df
+                     .apply(lambda row: row[column_name], axis=1)
+                     .apply(lambda target: re.sub(r'(\n|\r)+',r'', target))
+                     )
+    df[column_name] = stripped_column
+    return df
+
+def _tokenize_column(df,column_name):
+    
+        
+    logger.info(f'Counting meaningful words in {column_name}')
+    stop_words = set(stopwords.words('spanish'))
+    tokenize_column = (df.dropna()
+                            .apply(lambda row: nltk.word_tokenize(row[column_name]), axis=1)
+                            .apply(lambda tokens: list(filter(lambda token: token.isalpha(), tokens)))
+                            .apply(lambda tokens: list(map(lambda token: token.lower(),tokens)))
+                            .apply(lambda word_list: list(filter(lambda word: word not in stop_words, word_list)))
+                            .apply(lambda valid_word_list: len(valid_word_list)))
+    
+    df[f'n_tokens_{column_name}'] = tokenize_column
+     
+    return df
+
+def _save_data(df,filename):
+    clean_filename = 'clean_{}'.format(filename)
+    logger.info('Saving data at location: {}'.format(clean_filename))
+    df.to_csv(clean_filename)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename',
+                        help = 'The path to the dirty data',
+                        type= str)
+    
+    args = parser.parse_args()
+    df = main(args.filename)
+    
